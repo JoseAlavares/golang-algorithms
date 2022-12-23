4c46d92b033a445d21259e2011fba63c5da939fc
Jose Francisco <alvaresvaldes89@outlook.es>
2022-12-22 23:04:04 -0600

    Refactoring and improving functions using recursive algorithms and reducing the input data to increase the speed of the function

--- binary-search.go
+++ binary-search.go
@@ -25,13 +25,39 @@ func binary_search(input_value int, arr []int, min int, max int) (bool, int) {
 	return false, -1
 }
 
+func binary_search_reduce_array(arr []int, input_search int) (bool, int) {
+	sort.Ints(arr)
+	middle := len(arr) - 1
+
+	if input_search == arr[middle] {
+		return true, middle
+	}
+
+	if input_search > arr[middle] {
+		return binary_search_reduce_array(arr[middle + 1:len(arr) - 1], input_search)
+	} else if input_search < arr[middle] {
+		return binary_search_reduce_array(arr[0:middle - 1], input_search)
+	} else {
+		return false, -1
+	}
+}
+
 func main() {
+	input_search := 10
 	unordered_values := []int {0, 2, 10, 30, 24, 47, 100, 11, 37, 19, 34, 45, 78, 102, 23}
-	exists, index := binary_search(10, unordered_values, 0, len(unordered_values) - 1)
+	exists, index := binary_search(input_search, unordered_values, 0, len(unordered_values) - 1)
 
 	if exists {
 		fmt.Println("Exists the value in the position:", index + 1)
 	} else {
 		fmt.Println("Not exists the value")
+	}
+
+	exists_recursive, index_recursive := binary_search_reduce_array(unordered_values, input_search)
+
+	if exists_recursive {
+		fmt.Println("Exists the value recursive in the position:", index_recursive + 1)
+	} else {
+		fmt.Println("Not exists the value recursive")
 	}
 }
--- linear-search.go
+++ linear-search.go
@@ -14,13 +14,38 @@ func linear_search(input_search int) bool {
 	return false
 }
 
+func linear_search_recursive(arr []int, input_search int, start int, end int) bool {
+	if start > end {
+		return false
+	}
+
+	if input_search == arr[start] {
+		return true
+	} else {
+		return linear_search_recursive(arr, input_search, start + 1, end)
+	}
+
+}
+
 func main() {
-	exists_value := linear_search(31)
+	value_to_search := 11
+	exists_value := linear_search(value_to_search)
 
 	if exists_value {
 		fmt.Println("Exists the value")
 	} else {
 		fmt.Println("Not exists the value")
+	}
+
+	elements := []int {0, 2, 10, 30, 24, 47, 100, 11, 37, 19}
+	start := 0
+	end := len(elements) - 1
+	exists_value_recursive := linear_search_recursive(elements, value_to_search, start, end)
+
+	if exists_value_recursive {
+		fmt.Println("Exists the value in the recursive function")
+	} else {
+		fmt.Println("Not exists the value recursive")
 	}
 }
 
